913c3e7945582efb4dba4fb79d5beebb312f0cb8
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-09-20 23:25:08 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import AppNavigation from './navigation/AppNavigation';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <AppNavigation />
+    </>
+  );
+}
